d155a7bbbc90fad764e3afb97283620a54226464
Aidan Murray <aidanhansm@gmail.com>
2020-11-28 19:15:19 -0700

    Implement results method

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -14,13 +14,13 @@ class NytimesTest < Minitest::Test
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
     result = DataExtract.copyright(@hash)
-    puts result
 
     assert_equal result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
+    result = DataExtract.results(@hash)
 
     assert result.is_a? (Array)
     assert_equal 44, result.count
